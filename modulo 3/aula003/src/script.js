268fe817a5680f3b3500b02dc26aed13a9f04048
Augusto Brito <121031260+wgutto@users.noreply.github.com>
2025-01-09 20:10:18 -0300

    estudos

---
+++ modulo 3/aula003/src/script.js
@@ -0,0 +1,5 @@
+"use strict";
+const botao = document.querySelector('button');
+botao.addEventListener('click', () => {
+    console.log('oi');
+});
---
+++ modulo 3/aula006/public/assets/js/script.js
@@ -0,0 +1,5 @@
+"use strict";
+function somar(n1, n2) {
+    return n1 + n2;
+}
+console.log(somar(10, 24));
